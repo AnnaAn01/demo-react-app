4ded41adab10bc8754a6bddae50eba4c22220663
Anna An <arzumanyan.an@gmail.com>
2020-09-06 22:31:28 +0400

    components

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Summ from "./Example.js";
 import { multiply, sayHello } from "./Example.js";
 import DateAndTime from "./Moment.js";
 import Person from "./Person.js";
+import Product from "./Product.js";
 
 function App() {
   console.log(Summ(2, 4));
   console.log(multiply(2, 5));
-
   console.log(sayHello());
 
   return (
     <div className="App">
       <header className="App-header">
+        <Product name="orange" price="$4" description="Fresh oranges" />
         <Person name="Anna" />
         <DateAndTime />
         <h1>This is our first project</h1>
